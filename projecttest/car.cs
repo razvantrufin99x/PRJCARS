e8e0eb961b5e1853b14272d31381eb9ef899ddb0
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:37:07 +0200

    Add project files.

---
+++ projecttest/car.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projecttest001.projecttest
+{
+	public class car
+	{
+		public string type;
+		public Color _color;
+		public int numberOfWheels;
+		public int NumberOfSeats;
+		public string owner;
+		public string recordNumber;
+		public string id;
+
+		public car()
+		{
+			//throw new NotImplementedException();
+		}
+
+		public car(string ptype, Color pcolor, int pnrofwheels, int pnrofseats)
+		{
+			//throw new NotImplementedException();
+		}
+
+		~car()
+		{
+			//throw new NotImplementedException();
+		}
+	}
+}
